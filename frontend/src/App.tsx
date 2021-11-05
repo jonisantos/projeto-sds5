72efea24cf043d1736f8435307611323b017fddf
Jonilson dos Santos Nascimento <jonilson.nascimento@sescpantanal.com.br>
2021-11-05 11:36:49 -0400

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 
 function App() {
   return (
-    <div >
-      <h1 className="text-primary">Ola Mundo</h1>
-      <NavBar/>
+
+    <>
+    <NavBar/>
+    <div className="container">
+      <h1 className="text-primary">Olá Mundo</h1>
+
+      <DataTable/>
     </div>
+
+    <Footer/>
+
+    </>
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -12,5 +12,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
-
