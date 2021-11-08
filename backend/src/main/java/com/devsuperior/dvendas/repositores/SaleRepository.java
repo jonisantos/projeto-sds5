afdcacec436bb65d5b9d286a8a0fc92252edc44b
Jonilson dos Santos Nascimento <jonnyzerobravo@gmail.com>
2021-11-08 19:34:50 -0400

    sale

--- backend/src/main/java/com/devsuperior/dvendas/repositores/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dvendas/repositores/SaleRepository.java
@@ -1,9 +1,16 @@
 package com.devsuperior.dvendas.repositores;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dvendas.Entities.Sale;
+import com.devsuperior.dvendas.dto.SaleSumDTO;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+	@Query("SELECT new com.devsuperior.dvendas.dto.SaleDTO(obj.saller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
 }
--- backend/src/main/java/com/devsuperior/dvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dvendas/service/SaleService.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dvendas.Entities.Sale;
 import com.devsuperior.dvendas.dto.SaleDTO;
+import com.devsuperior.dvendas.dto.SaleSumDTO;
 import com.devsuperior.dvendas.repositores.SaleRepository;
 import com.devsuperior.dvendas.repositores.SellerRepository;
 
@@ -19,9 +23,15 @@ public class SaleService {
 	@Autowired
 	private SellerRepository sellerRepo;
 	
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable){
 		sellerRepo.findAll();
 		Page<Sale> result = repo.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repo.amountGroupedBySeller();
 	}
 }
